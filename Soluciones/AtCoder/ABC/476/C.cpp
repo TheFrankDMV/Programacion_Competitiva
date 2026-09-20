acf4c191b82d61d2ff13b4c5c0e9e52ab5989b11
Francisco_Martin <154648748+TheFrankDMV@users.noreply.github.com>
2026-09-19 19:02:32 -0600

    Create C.cpp

---
+++ Soluciones/AtCoder/ABC/476/C.cpp
@@ -0,0 +1,30 @@
+// AtCoder ABC 476 - C "Third Largest Number"
+// https://atcoder.jp/contests/abc476/tasks/abc476_c
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vll = vector<ll>;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+
+    ll n;
+    cin >> n;
+    vll A(n);
+    for(int i = 0; i < n; i++) cin >> A[i];
+
+    vll cur = {A[0], A[1], A[2]};
+    sort(cur.rbegin(), cur.rend());
+    cout << cur[2] << "\n";
+
+    for (int i = 3; i < n; i++) {
+        ll val = A[i];
+        for (int j = 0; j < 3; j++) {
+            if (val > cur[j]) swap(val, cur[j]);
+        }
+
+        cout << cur[2] << "\n";
+    }
+}
