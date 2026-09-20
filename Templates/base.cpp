e92a513962c6c9708a7f0c628d819e2a262f42a6
Francisco_Martin <154648748+TheFrankDMV@users.noreply.github.com>
2026-09-19 18:49:15 -0600

    Create base.cpp

---
+++ Templates/base.cpp
@@ -0,0 +1,14 @@
+//
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vll = vector<ll>;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+
+    
+}
