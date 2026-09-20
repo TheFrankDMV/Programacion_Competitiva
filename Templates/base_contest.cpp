35a241f2226d0d003299460be0b683f9e687e86c
Francisco_Martin <154648748+TheFrankDMV@users.noreply.github.com>
2026-09-19 18:50:13 -0600

    Create base_contest.cpp

---
+++ Templates/base_contest.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vll = vector<ll>;
+
+void solve() {
+
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+
+    ll tt = 1;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+}
