949046fcac19ba8f8985fb752786bc49dce407db
Francisco_Martin <154648748+TheFrankDMV@users.noreply.github.com>
2026-09-22 02:18:59 -0600

    Create A.cpp

---
+++ Soluciones/AtCoder/ABC/474/A.cpp
@@ -0,0 +1,17 @@
+// AtCoder ABC 475 - A "Not X"
+// https://atcoder.jp/contests/abc474/tasks/abc474_a
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vll = vector<ll>;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+
+    ll x;
+    cin >> x;
+
+    cout << (x == 1 ? 2 : 1) << "\n";
+}
