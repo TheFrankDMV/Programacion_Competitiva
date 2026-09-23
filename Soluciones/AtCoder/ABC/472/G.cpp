7631cc92b6ab13a02afdbfa7431484630e96c5de
Francisco_Martin <154648748+TheFrankDMV@users.noreply.github.com>
2026-09-22 18:59:52 -0600

    Create G.cpp

--- Soluciones/AtCoder/ABC/472/G.cpp
+++ Soluciones/AtCoder/ABC/472/G.cpp
@@ -31,7 +31,7 @@ struct MaxFlow {
             ll u = q.front(); q.pop();
             for (auto v : g[u]) {
                 if (cap[u][v] > 0 && from[v] == -1) {
-                    from[v]= u;
+                    from[v] = u;
                     q.push(v);
                     if (v == t) return true;
                 }
